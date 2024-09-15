70003b3c9f49b5aa50cc945a809c6259b8a82ada
ikomilzoda <ikomilzodi@ddn.com>
2024-09-15 14:13:20 +0500

    Init

---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,46 @@
+package handlers
+
+import (
+	"github.com/isoboi/bookings/pkg/config"
+	"github.com/isoboi/bookings/pkg/models"
+	"github.com/isoboi/bookings/pkg/render"
+	"net/http"
+)
+
+var Repo *Repository
+
+type Repository struct {
+	App *config.AppConfig
+}
+
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// Home us the home page handler
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	render.RenderTemplate(w, "home.page.gohtml", &models.TemplateData{})
+}
+
+// About is the about page handler
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again."
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+
+	stringMap["remote_ip"] = remoteIP
+
+	render.RenderTemplate(w, "about.page.gohtml", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
